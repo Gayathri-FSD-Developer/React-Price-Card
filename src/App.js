a29b5d6eceb775a8f50d2fef2642a5cd0aa5d306
Gayathri-FSD-Developer <gayathribalan52@gmail.com>
2024-01-18 21:47:35 +0530

    Updated the condition rendering

--- src/App.js
+++ src/App.js
@@ -1,72 +1,84 @@
-import './App.css';
-import Card from './Card';
+import "./App.css";
+import Card from "./Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // Hard copy data for process
-let price_details = [ 
-  { 
-    about:'Free',
-    month:'0',
-    available: false,
-    Features:[
-      ['fa-solid fa-check','Single User'],
-      ['fa-solid fa-check','50 GB storage'],
-      ['fa-solid fa-check','Unlimited public projects'],
-      ['fa-solid fa-check','Community access'],
-      ['fa-solid fa-xmark','Unlimted private project'],
-      ['fa-solid fa-xmark','Dedicated phone support'],
-      ['fa-solid fa-xmark','Free sub domain'],
-      ['fa-solid fa-xmark','Monthly status Report']
-    ]
-  },
+let price_details = [
   {
-    about:'PLUS',
-    month:'9',
-    available: false,
-    Features:[
-      ['fa-solid fa-check','Single User'],
-      ['fa-solid fa-check','50 GB storage'],
-      ['fa-solid fa-check','Unlimited public projects'],
-      ['fa-solid fa-check','Community access'],
-      ['fa-solid fa-check','Unlimted private project'],
-      ['fa-solid fa-check','Dedicated phone support'],
-      ['fa-solid fa-check','Free sub domain'],
-      ['fa-solid fa-xmark','Monthly status Report']
-    ]
-  },
-  {
-    about:'PRO',
-    month:'49',
+    about: "Free",
+    month: "0",
     available: true,
-    Features:[
-      ['fa-solid fa-check','Single User'],
-      ['fa-solid fa-check','50 GB storage'],
-      ['fa-solid fa-check','Unlimited public projects'],
-      ['fa-solid fa-check','Community access'],
-      ['fa-solid fa-check','Unlimted private project'],
-      ['fa-solid fa-check','Dedicated phone support'],
-      ['fa-solid fa-check','Free sub domain'],
-      ['fa-solid fa-check','Monthly status Report']
-    ]
-  }
+    Features: [
+      // ['fa-solid fa-check','Single User'],
+      // ['fa-solid fa-check','50 GB storage'],
+      // ['fa-solid fa-check','Unlimited public projects'],
+      // ['fa-solid fa-check','Community access'],
+      // ['fa-solid fa-xmark','Unlimted private project'],
+      // ['fa-solid fa-xmark','Dedicated phone support'],
+      // ['fa-solid fa-xmark','Free sub domain'],
+      // ['fa-solid fa-xmark','Monthly status Report']
+
+      { name: "Single User" },
+      { name: "50 GB storage" },
+      { name: "Unlimited public projects" },
+      { name: "Community access" },
+      { name: "Unlimted private project", enable: true },
+      { name: "Dedicated phone support", enable: true },
+      { name: "Free sub domain", enable: true },
+      { name: "Monthly status Report", enable: true },
+    ],
+  },
+  {
+    about: "PLUS",
+    month: "9",
+    available: true,
+    Features: [
+      { name: "Single User" },
+      { name: "50 GB storage" },
+      { name: "Unlimited public projects" },
+      { name: "Community access" },
+      { name: "Unlimted private project" },
+      { name: "Dedicated phone support" },
+      { name: "Free sub domain" },
+      { name: "Monthly status Report", enable: true },
+    ],
+  },
+  {
+    about: "PRO",
+    month: "49",
+    available: true,
+    Features: [
+      { name: "Single User" },
+      { name: "50 GB storage" },
+      { name: "Unlimited public projects" },
+      { name: "Community access" },
+      { name: "Unlimted private project" },
+      { name: "Dedicated phone support" },
+      { name: "Free sub domain" },
+      { name: "Monthly status Report" },
+    ],
+  },
 ];
 // Function to design the webpage with components
 function App() {
   return (
     <div className="container">
-    <div className='row'>
-      <div className='col-lg-4 col-md-6 col-sm-12 col-12'>
-        {/* destructing the array and passes the each elements 
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-12 col-12">
+          {/* destructing the array and passes the each elements 
         Assuming price_details[0] looks like { currency: 'USD', amount: 50 }, 
         the <Card> component will receive the following props: 
         like this=> <Card currency="USD" amount={50}></Card>
           */}
-      <Card {...price_details[0]}></Card></div>
-      <div className='col-lg-4 col-md-6 col-sm-12 col-12'>
-      <Card {...price_details[1]}></Card></div>
-      <div className='col-lg-4 col-md-6 col-sm-12 col-12'>
-      <Card {...price_details[2]}></Card></div>     
+          <Card {...price_details[0]}></Card>
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 col-12">
+          <Card {...price_details[1]}></Card>
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 col-12">
+          <Card {...price_details[2]}></Card>
+        </div>
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -10,12 +10,16 @@ export default function Card(props)
     <div className="card-body"> 
       {/* Instead the repetation of code can use Map method which returns the something for each element iteration*/}
         {
-          props.Features.map((ele,index)=>(
+          props.Features.map((ele,index)=>(        
+          //   <p className={`card-text ${ele.includes('fa-solid fa-xmark')?'opacity':''}`}>
+          //     <i className={ele[0]}></i> {ele[1]}
+          //  </p>
+
           //  Using this steps to adopt the css/ bootstrap style of our card. Fadded effect with "X" cross icon
-          //  Adds&concat the classname&icon by checks the mentioned values is there in array element using array includes method
-            <p className={`card-text ${ele.includes('fa-solid fa-xmark')?'opacity':''}`}>
-              <i className={ele[0]}></i> {ele[1]}
-           </p>
+          //  Add&concat the classname&icon by checks the mentioned values is there in array element using "enable" check.
+                  <p className={`card-text ${ele.enable?'opacity':''}`}>
+            <i className={ele.enable?'fa-solid fa-xmark':'fa-solid fa-check'}></i> {ele.name}
+          </p>
           ))
         }
         {/* Fade the button by specific value check */}
